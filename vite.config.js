ef5686e74cb1d6908d22459afbc5ae60ea298cf1
Arya Tino <aryatino>
2026-02-13 13:53:26 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        account: 'account.html',
+        checkout: 'checkout.html',
+        login: 'login.html',
+        register: 'register.html',
+        shop: 'shop.html',
+        view: 'view.html',
+        wishlist: 'wishlist.html'
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
